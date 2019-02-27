bee67e793b70a2e96c95fb94344f4e647bfc2861
ramakanth.b <ramakanth.b@BLR-NB0020>
2019-02-27 12:09:52 +0530

    Jersey commit 1

---
+++ springboot-jersey-demo/src/main/java/com/kanth/springbootjerseydemo/SpringbootJerseyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kanth.springbootjerseydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJerseyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJerseyDemoApplication.class, args);
+	}
+
+}
---
+++ springboot-jersey-demo/src/test/java/com/kanth/springbootjerseydemo/SpringbootJerseyDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kanth.springbootjerseydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootJerseyDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
